f2fba040509d5c6e787300f98b72af0c0fe08511
Charis <charisma1988sc@gmail.com>
2019-04-26 00:41:13 +0800

    Update ReflectiveMethodInvocationTest.java

--- litespring/src/test/java/org/litespring/test/v5/ReflectiveMethodInvocationTest.java
+++ litespring/src/test/java/org/litespring/test/v5/ReflectiveMethodInvocationTest.java
@@ -29,7 +29,7 @@ public class ReflectiveMethodInvocationTest {
 	private TransactionManager tx;
 	
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		petStore = new PetStoreService();
 		MessageTracker.clearMsgs();
 		beforeAdvice = new AspectJBeforeAdvice(TransactionManager.class.getMethod("start"), null, tx);
